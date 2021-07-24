76c41aab00e9e5e717c11782f1b425cf4d3d5674
farrelf20 <ianjulian321@gmail.com>
2021-07-24 15:51:32 +0700

    add api covid19 and api rujukan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CovidController;
+use App\Http\Controllers\RujukanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +20,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/home', [HomeController::class, 'index']);
+Route::get('api/covid', [CovidController::class, 'index']);
+Route::get('api/rujukan', [RujukanController::class, 'index']);
